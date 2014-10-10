abe876578027fc0fc1cc598088afaec1731cebe2
Ricardo Haas <ricardo@voltsdigital.com.br>
2014-10-10 10:34:49 -0300

    Exemplo usando MTB_Page

---
+++ metaboxes/mtb-advantages.php
@@ -0,0 +1,41 @@
+<?php
+
+class MTB_Advantages Extends MTB_Page{
+
+    public function setup_metaboxes(){
+
+        $advantages = new Odin_Metabox(
+            'advantages_us_info', // Slug/ID do Metabox (obrigatório)
+            'Serviços', // Nome do Metabox  (obrigatório)
+            'page', // Slug do Post Type (opcional)
+            'normal', // Contexto (opções: normal, advanced, ou side) (opcional)
+            'high' // Prioridade (opções: high, core, default ou low) (opcional)
+        );
+
+        $advantages->set_fields(
+            array(
+                array(
+                    'id'          => 'advantages_content',
+                    'label'        => 'Descrição',
+                    'type'        => 'editor', // Obrigatório
+                    'options'     => array(
+                            'textarea_rows' => 5,
+                            'media_buttons' => false
+                        ),
+                ),
+            )
+        );
+    }
+
+    public function setup_editor_settings( $settings ) {
+        $style_formats = array();
+        $settings['style_formats'] = json_encode( $style_formats );
+        $toolbar1 = 'bold,italic,bullist';
+        $settings['toolbar1'] = $toolbar1;
+        $settings['toolbar2'] = '';
+        return $settings;
+    }
+
+}
+
+$mtb_advantages_us = new MTB_Advantages( 'page-vantagens.php' );
