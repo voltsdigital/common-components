bba3bf276cdd8eedac4fae51f01b8bc8ebfdfc69
Luis Felipe de Adrade <luis@voltsdigital.com.br>
2014-10-10 11:04:30 -0300

    [ CONTATO ] Converte para formato sem apply_filters

--- controllers/ctr-contact.php
+++ controllers/ctr-contact.php
@@ -5,7 +5,7 @@ if( !defined( 'WPINC' ) )
 
 /**
  *
- * Controller Contato
+ * Controller Contact
  */
 class CTR_Contact
 {
@@ -14,17 +14,15 @@ class CTR_Contact
 
     // -----------------------------------------------------------------------------
 
-    function __construct() {
-        add_filter( "ctr_contact_send_mail" ,  array( &$this, 'send_mail' ), 10, 2);
-    }
+    function __construct() { }
 
     // -----------------------------------------------------------------------------
 
     /**
-     * Send a contact mail 
+     * Send a contact mail
      * @return bool True if sucess, false if fail
      */
-    public function send_mail() {
+    public function sendMail() {
 
         if(!$_POST)
             return false;
@@ -33,14 +31,14 @@ class CTR_Contact
 
         if( isset( $contact_info['email_contato'] ) )
             $this->_mail_contact = $contact_info['email_contato'];
-
-        $headers[] = "From: Cliente - Página de Contato  <". $this->_mail_contact . ">";
+        
+        $headers[] = "From: MS Shirt - Página de Contato  <". $this->_mail_contact . ">";
         $headers[] = 'Cco: Luís Felipe de Andrade <luis@voltsdigital.com.br>';
         $headers[] = "Content-type: text/html";
 
         $body      = $this->formatBodyContent();
 
-        if( wp_mail( $this->_mail_contact  ,  " Cliente - Página de Contato ", $body, $headers) )
+        if( wp_mail( $this->_mail_contact  ,  " MS Shirt - Página de Contato ", $body, $headers) )
             return true;
         else
             return false;
@@ -51,21 +49,19 @@ class CTR_Contact
 
     /**
      * Format the body content of the mail
-     * @return string 
+     * @return string
      */
     private function formatBodyContent() {
 
         $data = new DateTime("now",  new DateTimeZone('America/Sao_Paulo'));
-        $body = file_get_contents(TEMPLATEPATH . '/partials/mail-template/mail-template-contact.php');
-        $body = str_replace( "%name%",  $_POST['cf_name'], $body);
+        $body = file_get_contents(TEMPLATEPATH . '/partials/mail-template/contact.php');
+        $body = str_replace( "%name%",  $_POST['fc_name'], $body);
         $body = str_replace( "%titulo%", 'E-mail enviado via Página de Contato', $body);
-        $body = str_replace( "%email%", $_POST['cf_email'], $body);
-        $body = str_replace( "%telephone%", $_POST['cf_phone'], $body);
-        $body = str_replace( "%message%", $_POST['cf_message'], $body);
-        $body = str_replace( "%subscribe_news%", $_POST['cf_subscribe_news'], $body);
+        $body = str_replace( "%email%", $_POST['fc_email'], $body);
+        $body = str_replace( "%telephone%", $_POST['fc_phone'], $body);
+        $body = str_replace( "%message%", $_POST['fc_message'], $body);
         $body = str_replace( "%sent_date%", $data->format("d/m/Y H:i:s"), $body );
         return $body;
-
     }
 
     // -----------------------------------------------------------------------------
