2ba7f07bcb6f5fd787c4b242790afc8c4165b90d
Luis Felipe de Adrade <luis@voltsdigital.com.br>
2015-08-24 14:39:33 -0300

    [ BANNER ] Atualiza o componente padrão do banner

--- controllers/ctr-banner.php
+++ controllers/ctr-banner.php
@@ -7,18 +7,16 @@ if( !defined( 'WPINC' ) )
  *
  * All the functions used in the front end are used by this controller
  */
-class CTR_Banner
+class CTRBanner
 {
 
     // Redimensionamento
-    private static $_width  = 960;
-    private static $_height = 353;
+    public $width  = 960;
+    public $height = 353;
 
     // -----------------------------------------------------------------------------
 
-    function __construct() {
-        add_filter( "ctr_get_banners_home", array(&$this, 'get_banners_home'), 10, 2);
-    }
+    function __construct() { }
 
     // -----------------------------------------------------------------------------
 
@@ -27,29 +25,29 @@ class CTR_Banner
      * @return  array Array de Objetos
      */
 
-
-    public function get_banners_home() {
+    public function getImages() {
 
         $args = array(
-            'post_type'      => 'banner',
+            'post_type'      => 'banner_home',
             'posts_per_page' => -1,
             'orderby'        => 'menu_order'
         );
+
         $banners = get_posts( $args );
+
         foreach($banners as $banner) {
-            $id_image          = get_post_meta( $banner->ID,  "banner_foto" , "single" );
-            $path_image        = wp_get_attachment_url( $id_image );
-            $banner->image_src = $path_image;
-            $banner->link      = get_post_meta( $banner->ID,  "banner_link" , "single" ) ;
-            if(!strstr( $banner->link, "http://"))
+
+            $id_image         = get_post_meta( $banner->ID,  "banner_foto" , "single" );
+            $banner->imageSrc = wp_get_attachment_url( $id_image );
+            $banner->link     = get_post_meta( $banner->ID,  "banner_link" , "single" ) ;
+            $banner->alt      = get_post_meta( $id_image, '_wp_attachment_image_alt', true);
+
+            if(!strstr( $banner->link, "http://") && $banner->link)
                 $banner->link = 'http://' . $banner->link;
-            $banner->alt       = get_post_meta( $id_image, '_wp_attachment_image_alt', true);
-            $banner->cor_fundo = get_post_meta( $banner->ID,  'banner_cor_fundo', true );
         }
+
         return $banners;
     }
 
     // -----------------------------------------------------------------------------
 }
-
-new CTR_Banner;
